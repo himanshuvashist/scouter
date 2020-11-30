8b234cfb3dd5bf126b7676a371f5410881f53688
himanshuvashist <35025815+himanshuvashist@users.noreply.github.com>
2020-11-30 23:49:47 +0530

    give logout functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,25 @@ import Form from './form/Form'
 import Tabs from './Tabs'
 import List from './list/List'
 import { connect } from 'react-redux'
-
+import { Button, Grid } from '@material-ui/core'
+import { updateSignedInState } from './../actions/updateSignedInState'
 export function Home(props) {
+    const handleLogout = () => {
+        props.updateState('')
+    }
     return (
         <div>
             <br />
             <Tabs />
+            <br />
+            <div className="logoutButton">
+                <Grid container spacing={1} justify="center" alignItems="center">
+                    <Button variant="outlined" color="secondary" onClick={handleLogout}>
+                        Logout
+                    </Button>
+                </Grid>
+            </div>
+
             {props.state.activePage ? <List /> : <Form />}
         </div>
     )
@@ -19,4 +32,12 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps)(Home)
+const mapDispatchToProps = (dispatch) => {
+    return {
+        updateState: (data) => {
+            return dispatch(updateSignedInState(data))
+        },
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
